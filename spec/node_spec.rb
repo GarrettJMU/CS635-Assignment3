8d202fa8a017d44a8865bdb344d8d241d5cd7158
GarrettJMU <garretthughes3@gmail.com>
2019-11-23 17:04:07 -0800

    back to green

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,7 +2,16 @@ require 'spec_helper'
 require_relative '../lib/node'
 
 RSpec.describe Node do
-  it 'should work' do
-    expect(true).to eq(true)
+  let(:value) { nil }
+  subject { Node.new(value) }
+
+  describe '#new' do
+    context 'when a value is passed' do
+      let(:value) { "foo" }
+      it 'should store the value in the attributes' do
+        expect(subject.value).to eq("foo")
+      end
+    end
+
   end
 end
