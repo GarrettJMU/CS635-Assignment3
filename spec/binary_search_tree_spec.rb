159e8d79a202320008a8c98dd9827a7e71e04a9e
GarrettJMU <garretthughes3@gmail.com>
2019-11-24 15:15:30 -0800

    more green

--- spec/binary_search_tree_spec.rb
+++ spec/binary_search_tree_spec.rb
@@ -37,34 +37,34 @@ RSpec.describe BinarySearchTree do
       end
     end
   end
-  #
-  #
-  # describe '#visualize' do
-  #   context 'when there are no nodes' do
-  #     it 'should return ()' do
-  #       expect(subject.visualize).to eq("()")
-  #     end
-  #   end
-  #
-  #   context 'when there are nodes' do
-  #     # it 'should return a visualization' do
-  #     #   subject.insert("foo")
-  #     #   expect(subject.root.value).to eq("foo")
-  #     #   expect(subject.visualize).to eq("(foo () ())")
-  #     # end
-  #
-  #     it 'should show the entire representation of the node' do
-  #       subject.insert("b")
-  #       subject.insert("a")
-  #       subject.insert("c")
-  #
-  #       expect(subject.root.value).to eq("b")
-  #       expect(subject.root.left.value).to eq("a")
-  #       expect(subject.root.right.value).to eq("c")
-  #       # expect(subject.visualize).to eq("(b (a () ()) (c () ()))")
-  #     end
-  #   end
-  # end
+
+
+  describe '#visualize' do
+    context 'when there are no nodes' do
+      it 'should return ()' do
+        expect(subject.visualize).to eq("()")
+      end
+    end
+
+    context 'when there are nodes' do
+      # it 'should return a visualization' do
+      #   subject.insert("foo")
+      #   expect(subject.root.value).to eq("foo")
+      #   expect(subject.visualize).to eq("(foo () ())")
+      # end
+
+      it 'should show the entire representation of the node' do
+        subject.insert("b")
+        subject.insert("a")
+        subject.insert("c")
+
+        expect(subject.root.value).to eq("b")
+        expect(subject.root.left.value).to eq("a")
+        expect(subject.root.right.value).to eq("c")
+        # expect(subject.visualize).to eq("(b (a () ()) (c () ()))")
+      end
+    end
+  end
   #
   # describe '#search' do
   #   context 'when there are values' do
